44af9ae8178ee78b62362c93f38be7cf8806bb2b
15017082 <15017082@myrp.edu.sg>
2017-07-18 00:40:26 +0800

    Juble

--- app/src/main/java/com/example/a15041867/vms/User.java
+++ app/src/main/java/com/example/a15041867/vms/User.java
@@ -1,5 +1,11 @@
 package com.example.a15041867.vms;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
 /**
  * Created by 15017082 on 10/7/2017.
  */
@@ -14,41 +20,57 @@ public class User {
     private String unit;
     private String password;
 
-    public User(String user_email, String name, String handphone_number, String position, String block, String unit, String password  ){
-        this.user_email = user_email;
-        this.name = name;
-        this.handphone_number = handphone_number;
-        this.position = position;
-        this.block = block;
-        this.unit = unit;
-        this.password = password;
+    public User(){
+        super();
     }
 
     public String getUser_email(){
         return user_email;
     }
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
 
     public String getName(){
         return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getHandphone_number(){
         return handphone_number;
     }
+    public void setHandphone_number(String handphone_number) {
+        this.handphone_number = handphone_number;
+    }
 
     public String getPosition(){
         return position;
+    }
+    public void setPosition(String position) {
+        this.position = position;
     }
 
     public String getBlock(){
         return block;
     }
+    public void setBlock(String block) {
+        this.block = block;
+    }
 
     public String getUnit(){
         return unit;
+    }
+    public void setUnit(String unit) {
+        this.unit = unit;
     }
 
     public String getPassword(){
         return password;
     }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
--- app/src/main/java/com/example/a15041867/vms/UserInfoActivity.java
+++ app/src/main/java/com/example/a15041867/vms/UserInfoActivity.java
@@ -1,15 +1,29 @@
 package com.example.a15041867.vms;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class UserInfoActivity extends AppCompatActivity {
+
+    private static final String TAG = "UserInfoActivity";
 
     private Session session;
     private DrawerLayout mDrawerLayout;
@@ -17,6 +31,10 @@ public class UserInfoActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private NavigationView nv;
     Intent i;
+    String loginId, apikey;
+
+    ListView lv;
+    ArrayList<User> userList = new ArrayList<User>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +46,11 @@ public class UserInfoActivity extends AppCompatActivity {
 //            logout();
 //        }
 
-        nv = (NavigationView)findViewById(R.id.nvManager);
+        nv = (NavigationView) findViewById(R.id.nvManager);
 //        mToolbar = (Toolbar)findViewById(R.id.nav_action);
 //        setSupportActionBar(mToolbar);
 
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawerLayoutUserInfo);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayoutUserInfo);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
 
         mDrawerLayout.addDrawerListener(mToggle);
@@ -44,33 +62,111 @@ public class UserInfoActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
-                    case(R.id.nav_visitor_info):
-                        i = new Intent(getApplicationContext(),VisitorInfoActivity.class);
+                    case (R.id.nav_visitor_info):
+                        i = new Intent(getApplicationContext(), VisitorInfoActivity.class);
                         startActivity(i);
                         break;
-                    case(R.id.nav_summary):
-                        i= new Intent(getApplicationContext(),SummaryActivity.class);
+                    case (R.id.nav_summary):
+                        i = new Intent(getApplicationContext(), SummaryActivity.class);
                         startActivity(i);
                         break;
-                    case(R.id.nav_evacuation):
-                        i= new Intent(getApplicationContext(),EvacuationActivity.class);
+                    case (R.id.nav_evacuation):
+                        i = new Intent(getApplicationContext(), EvacuationActivity.class);
                         startActivity(i);
                         break;
-                    case(R.id.nav_add_user):
-                        i= new Intent(getApplicationContext(),AddUserActivity.class);
+                    case (R.id.nav_add_user):
+                        i = new Intent(getApplicationContext(), AddUserActivity.class);
                         startActivity(i);
                         break;
-                    case(R.id.nav_edit_user):
-                        i= new Intent(getApplicationContext(),UserInfoActivity.class);
+                    case (R.id.nav_edit_user):
+                        i = new Intent(getApplicationContext(), UserInfoActivity.class);
                         startActivity(i);
                         break;
-                    case(R.id.log_out):
-                        i= new Intent(getApplicationContext(),MainActivity.class);
+                    case (R.id.log_out):
+                        i = new Intent(getApplicationContext(), MainActivity.class);
                         startActivity(i);
                         break;
                 }
                 return true;
             }
         });
+
+
+        userList.clear();
+        // Check if there is network access
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+
+            //TODO 03 Extract the loginId and API Key from the intent object
+            i = getIntent();
+            loginId = i.getStringExtra("loginId");
+            apikey = i.getStringExtra("apikey");
+            /******************************/
+            if (apikey != null) {
+                HttpRequest request = new HttpRequest("https://ruixian-ang97.000webhostapp.com/getUser.php");
+                request.setMethod("POST");
+                request.addData("loginId", loginId);
+                request.addData("apikey", apikey);
+                request.execute();
+
+                try {
+                    String jsonString = request.getResponse();
+                    Log.d(TAG, "jsonString: " + jsonString);
+
+                    JSONArray jsonArray = new JSONArray(jsonString);
+
+                    // Populate the arraylist personList
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jObj = jsonArray.getJSONObject(i);
+                        User user = new User();
+                        user.setUser_email(jObj.getString("user_email"));
+                        user.setName(jObj.getString("name"));
+                        user.setHandphone_number(jObj.getString("handphone_number"));
+                        user.setPosition(jObj.getString("position"));
+                        user.setBlock(jObj.getString("block"));
+                        user.setUnit(jObj.getString("unit"));
+                        user.setPassword(jObj.getString("password"));
+                        userList.add(user);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                UserArrayAdapter arrayAdapter = new UserArrayAdapter(this, R.layout.row_user, userList);
+                lv = (ListView) findViewById(R.id.lvUserInfo);
+                lv.setAdapter(arrayAdapter);
+
+            } else {
+                // AlertBox
+                showAlert("Login Failed");
+            }
+        } else {
+            // AlertBox
+            showAlert("No network connection!");
+        }
+    }
+
+    private void showAlert(String msg){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(msg)
+                .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // if this button is clicked, close
+                        // current activity
+                        UserInfoActivity.this.finish();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = builder.create();
+
+        // show it
+        alertDialog.show();
     }
 }
+
+
+
